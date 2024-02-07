f6f5a55412e79a288d27b9615f50a7d59860204c
Toby Clark <psytc9@nottingham.ac.uk>
2024-02-07 15:27:26 +0000

    add: further list support (incomplete)

--- wjson.c
+++ wjson.c
@@ -84,7 +84,6 @@ void wjson_print(struct wjson* head) {
 
 
 
-
 struct wjson* wjson_initialize()
 {
     /* Allocating Memory for new struct wjson pointer */
@@ -389,6 +388,129 @@ int wjson_list_append_object(struct wjson* wjson_node, struct wjson* value)
     return 1;
 }
 
+int wjson_list_append_numerical(struct wjson* wjson_node, double value)
+{
+    /* Go to the end of the list */
+    int index = 0;
+    while (wjson_node->next != NULL)
+    {
+        index++;
+        wjson_node = wjson_node->next;
+    }
+
+    /* Node is empty (first element in the list) */
+    struct wjson* new_node;
+    if (wjson_node->type == WJSON_TYPE_EMPTY)
+        new_node = wjson_node;
+    else
+    {
+        new_node = wjson_initialize();
+        wjson_node->next = new_node;
+        new_node->prev = wjson_node;
+    }
+
+    new_node->type = WJSON_TYPE_NUMERICAL;
+
+    /* Converting index to string */
+    int bufferSize = snprintf(NULL, 0, L"%d", index);
+    wchar_t *buffer = (wchar_t *)malloc((bufferSize + 1) * sizeof(wchar_t));
+    swprintf(buffer, bufferSize + 1, L"%d", index);
+    new_node->key = wcsdup(buffer);
+
+    if (new_node->key == NULL)
+    {
+        fprintf(stderr, "wJson: Failed to allocate memory for new wJson Key.");
+        free(new_node->key);
+        return 0;
+    }
+
+    new_node->data_numerical = value;
+
+    return 1;
+}
+
+int wjson_list_append_list(struct wjson* wjson_node, struct wjson* value)
+{
+    /* Go to the end of the list */
+    int index = 0;
+    while (wjson_node->next != NULL)
+    {
+        index++;
+        wjson_node = wjson_node->next;
+    }
+
+    /* Node is empty (first element in the list) */
+    struct wjson* new_node;
+    if (wjson_node->type == WJSON_TYPE_EMPTY)
+        new_node = wjson_node;
+    else
+    {
+        new_node = wjson_initialize();
+        wjson_node->next = new_node;
+        new_node->prev = wjson_node;
+    }
+
+    new_node->type = WJSON_TYPE_LIST;
+
+    /* Converting index to string */
+    int bufferSize = snprintf(NULL, 0, L"%d", index);
+    wchar_t *buffer = (wchar_t *)malloc((bufferSize + 1) * sizeof(wchar_t));
+    swprintf(buffer, bufferSize + 1, L"%d", index);
+    new_node->key = wcsdup(buffer);
+
+    if (new_node->key == NULL)
+    {
+        fprintf(stderr, "wJson: Failed to allocate memory for new wJson Key.");
+        free(new_node->key);
+        return 0;
+    }
+
+    new_node->data_list = value;
+
+    return 1;
+}
+
+int wjson_list_append_boolean(struct wjson* wjson_node, bool value)
+{
+    /* Go to the end of the list */
+    int index = 0;
+    while (wjson_node->next != NULL)
+    {
+        index++;
+        wjson_node = wjson_node->next;
+    }
+
+    /* Node is empty (first element in the list) */
+    struct wjson* new_node;
+    if (wjson_node->type == WJSON_TYPE_EMPTY)
+        new_node = wjson_node;
+    else
+    {
+        new_node = wjson_initialize();
+        wjson_node->next = new_node;
+        new_node->prev = wjson_node;
+    }
+
+    new_node->type = WJSON_TYPE_BOOLEAN;
+
+    /* Converting index to string */
+    int bufferSize = snprintf(NULL, 0, L"%d", index);
+    wchar_t *buffer = (wchar_t *)malloc((bufferSize + 1) * sizeof(wchar_t));
+    swprintf(buffer, bufferSize + 1, L"%d", index);
+    new_node->key = wcsdup(buffer);
+
+    if (new_node->key == NULL)
+    {
+        fprintf(stderr, "wJson: Failed to allocate memory for new wJson Key.");
+        free(new_node->key);
+        return 0;
+    }
+
+    new_node->data_bool = value;
+
+    return 1;
+}
+
 
 
 
@@ -401,7 +523,9 @@ void wjson_test()
     wjson_append_string(test, L"Key3", L"Val3");
 
     struct wjson* test2 = wjson_initialize_list();
-    wjson_list_append_string(test2, L"2Val1");
+    wjson_list_append_string(test2, L"Val1");
+    wjson_list_append_numerical(test2, 32);
+    wjson_list_append_string(test2, L"Val3");
 
     wjson_append_list(test, "Key4", test2);
 
